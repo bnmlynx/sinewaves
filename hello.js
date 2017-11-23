b5be70217cc718f790acc64c4ef0a7bf5f6d4ca0
Ben Molyneux <bnmlynx@gmail.com>
2017-11-23 14:56:04 +0000

    Add files via upload

--- hello.js
+++ hello.js
@@ -11,14 +11,14 @@ Module.register("helloworld",{
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = this.config.text;
 		return wrapper;
-	}
+	},
 
 	getScripts: function() {
-	return [
-		'sine-waves.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-		//'sine-waves.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-		this.file('sine-waves.js'), // this file will be loaded straight from the module folder.
-		//'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-	]
-}
+		return [
+			'sine-waves.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+			//'sine-waves.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
+			this.file('sine-waves.js'), // this file will be loaded straight from the module folder.
+			//'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
+		]
+	},
 });
